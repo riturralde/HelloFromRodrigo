3dc32e2b2181577460f86d00c26b08ab0d695bb9
chasemoses <43392487+chasemoses@users.noreply.github.com>
2020-05-12 12:59:48 -0600

    Chase added a response.

--- src/team7/Main.java
+++ src/team7/Main.java
@@ -3,6 +3,9 @@ package team7;
 public class Main {
 
     public static void main(String[] args) {
-	      System.out.println("Hello, my name is Rodrigo");
+
+        System.out.println("Hello, my name is Rodrigo");
+
+        System.out.println("\nHello Rodrigo, my name is Chase.");
     }
 }
